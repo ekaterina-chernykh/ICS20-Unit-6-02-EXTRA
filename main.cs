0329ed910440566f310c17125687c0772794b34a
ekaterina-chern <113055918+ekaterina-chernykh@users.noreply.github.com>
2023-01-03 01:07:01 +0000

    whitespaces

--- main.cs
+++ main.cs
@@ -13,7 +13,6 @@ class Program
     {
         string choice;
         int counter = 0;
-        
         Console.WriteLine("Do you want to quit? ");
         Console.WriteLine(" ");
         Console.WriteLine("Type Yes or No ");
@@ -22,11 +21,11 @@ class Program
         choice = Console.ReadLine();
         if (choice == "no" || choice == "No")
         {
-          var someText = await File.ReadAllTextAsync("LocalStorage.txt");
-          counter = Convert.ToInt16(someText);
-          counter++;
-          await File.WriteAllTextAsync("LocalStorage.txt", counter.ToString());
-          Console.WriteLine(counter.ToString());
+            var someText = await File.ReadAllTextAsync("LocalStorage.txt");
+            counter = Convert.ToInt16(someText);
+            counter++;
+            await File.WriteAllTextAsync("LocalStorage.txt", counter.ToString());
+            Console.WriteLine(counter.ToString());
         }
 
         Console.WriteLine("\nDone.");
