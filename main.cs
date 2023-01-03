7f82be3ef0f643be143b7eb055e3a37ac9e46242
ekaterina-chern <113055918+ekaterina-chernykh@users.noreply.github.com>
2023-01-03 00:41:04 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,33 @@
+// Created by: Ekaterina
+// Created on: Jan 2023
+//
+// This program does simple long division
+
+using System;
+using System.Threading.Tasks;
+using System.IO;
+
+class Program
+{
+    public static async Task Main()
+    {
+      string choice;
+      int counter = 0;
+      
+      Console.WriteLine("Do you want to quit? ");
+      Console.WriteLine(" ");
+      Console.WriteLine("Type Yes or No ");
+      Console.WriteLine(" ");
+
+      choice = Console.ReadLine();
+      if (choice == "no" || choice == "No") {
+        var someText = await File.ReadAllTextAsync("LocalStorage.txt");
+        counter = Convert.ToInt16(someText);
+        counter++;
+        await File.WriteAllTextAsync("LocalStorage.txt", counter.ToString());
+        Console.WriteLine(counter.ToString());
+      }
+
+      Console.WriteLine("\nDone.");
+    }
+}
